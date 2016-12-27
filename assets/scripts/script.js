edb4d2570e678a96dd2ffe4961660edda6123500
Ian Adams <ian.adams@ge.com>
2016-12-27 17:43:00 -0600

    cast and crew json

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $('.parallax').parallax();
     $('section').scrollSpy({scrollOffset:0});
+
+    //add elipses based on text length
+    $('#section6 ul li').each(function(index){
+        n = 50 - $(this).find('span.member').text().length + $(this).find('span.character').text().length;
+        $(this).find('span.member').append('.'.repeat(n));
+    });
 });
        
